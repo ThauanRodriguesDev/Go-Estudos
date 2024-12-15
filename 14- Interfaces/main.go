36521a1625c9199b54eece7f386996caf35d9c19
Thauan <dev.thauan.rodrigues@gmail.com>
2024-12-15 00:17:01 -0300

    Interfaces em Go

---
+++ 14- Interfaces/main.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+type Endereço struct {
+	Logradouro string
+	Numero     int
+	Cidade     string
+	Estado     string
+}
+
+type Pessoa interface {
+	Desativar()
+}
+
+type Cliente struct {
+	Nome  string
+	Idade int
+	Ativo bool
+	Endereço
+}
+
+type Empresa struct {
+	Nome string
+}
+
+func (c Cliente) Desativar() {
+	c.Ativo = false
+	fmt.Printf("O Cliente foi desativado\n")
+}
+
+func (e Empresa) Desativar() {
+}
+
+func Desativacao(pessoa Pessoa) {
+	pessoa.Desativar()
+}
+
+func main() {
+
+	thauan := Cliente{
+		Nome:  "Thauan",
+		Idade: 30,
+		Ativo: true,
+	}
+	minhaEmpresa := Empresa{}
+
+	Desativacao(minhaEmpresa)
+}
