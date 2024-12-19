781c171074167942c4f4a243f7a1cfc94ad1c800
Thauan <dev.thauan.rodrigues@gmail.com>
2024-12-19 20:33:10 -0300

    Interfaces Vazias

---
+++ 17- Ponteiros e Structs/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+type Client struct {
+	nome string
+}
+
+func NewClient() *Client {
+	return &Client{nome: ""}
+}
+func (c *Client) andou() {
+	c.nome = "Thauan Rodrigues"
+	fmt.Printf("O cliente %v andou\n", c.nome)
+}
+
+func main() {
+	thauan := Client{
+		nome: "thauan",
+	}
+	thauan.andou()
+	fmt.Printf("O valor da struct com nome é %v\n", thauan.nome)
+}
