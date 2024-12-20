307b4559830d4eacbd30a10e2b7f3eec8afa5d7e
Thauan <dev.thauan.rodrigues@gmail.com>
2024-12-20 10:32:39 -0300

    Instalando Pacotes

--- 21- Pacotes e Modulos/main.go
+++ 21- Pacotes e Modulos/main.go
@@ -8,5 +8,9 @@ import (
 func main() {
 	s := matematica.Soma(10, 20)
 
+	carro := matematica.Carro{Marca: "Fiat"}
+
+	fmt.Println(carro.Marca)
 	fmt.Println(s)
+	fmt.Println(matematica.A)
 }
--- 21- Pacotes e Modulos/matematica/matematica.go
+++ 21- Pacotes e Modulos/matematica/matematica.go
@@ -1,5 +1,13 @@
 package matematica
 
+//Tudo que começa com letra maiuscula esta Exportado, tudo que começa com letra minuscula nao esta exportado
+
 func Soma[T int | float64](a, b T) T {
 	return a + b
+}
+
+var A int = 10
+
+type Carro struct {
+	Marca string
 }
