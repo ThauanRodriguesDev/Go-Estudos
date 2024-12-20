ffb8c6df32aa39758425bf00d1e2e8023b312ca5
Thauan <dev.thauan.rodrigues@gmail.com>
2024-12-20 10:41:57 -0300

    Looping no Go

---
+++ 23- Loopings no Go/main.go
@@ -0,0 +1,24 @@
+package main
+
+func main() {
+
+	// for i := 0; i < 10; i++ { // -> looping padrao
+	// 	println(i)
+	// }
+
+	// numeros := []string{"um", "dois", "tres"} // -> Slice
+
+	// for k, v := range numeros {
+	// 	println(k, v)
+	// }
+
+	// i := 0
+	// for i < 10 {   // -> looping similar ao while
+	// 	println(i)
+	// 	i++
+	// }
+
+	// for {     // -> looping infinito
+	// 	println("JEsus te AMa")
+	// }
+}
