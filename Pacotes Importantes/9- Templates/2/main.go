d84925a13a089aaf5aaeaafce4c8a72bfa409970
Thauan <dev.thauan.rodrigues@gmail.com>
2024-12-30 00:40:45 -0300

    HttpClient

--- Pacotes Importantes/9- Templates/2/main.go
+++ Pacotes Importantes/9- Templates/2/main.go
@@ -12,11 +12,9 @@ type Curso struct {
 
 func main() {
 	curso := Curso{"Go", 40}
-	tmp := template.New("CursoTemplate")
+	t := template.Must(template.New("CursoTemplate").Parse("Curso: {{.Nome}} - Carga Horária: {{.CargaHoraria}}"))
 
-	tmp, _ = tmp.Parse("Curso: {{.Nome}} - Carga Horária: {{.CargaHoraria}}")
-
-	err := tmp.Execute(os.Stdout, curso)
+	err := t.Execute(os.Stdout, curso)
 
 	if err != nil {
 		panic(err)
