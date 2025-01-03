73c14d68a1a5f1d3f9ae7b1e270420359abbf8a4
Thauan <dev.thauan.rodrigues@gmail.com>
2025-01-03 17:59:44 -0300

    Contexto na pratica

--- Pacotes Importantes/10- HttpClient/3/main.go
+++ Pacotes Importantes/10- HttpClient/3/main.go
@@ -1,30 +1,33 @@
 package main
 
 import (
+	"context"
 	"io"
 	"net/http"
+	"time"
 )
 
 func main() {
-	c := http.Client{}
-	req, err := http.NewRequest("GET", "http://google.com", nil)
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, time.Microsecond)
+	defer cancel()
+	req, err := http.NewRequestWithContext(ctx, "GET", "http://google.com", nil)
 
 	if err != nil {
 		panic(err)
 	}
-
-	req.Header.Set("Accept", "application/json")
-
-	resp, err := c.Do(req)
+	resp, err := http.DefaultClient.Do(req)
 
 	if err != nil {
 		panic(err)
 	}
-
 	defer resp.Body.Close()
+
 	body, err := io.ReadAll(resp.Body)
+
 	if err != nil {
 		panic(err)
 	}
+
 	println(string(body))
 }
