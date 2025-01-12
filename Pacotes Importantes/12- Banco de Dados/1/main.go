b03ebd46851acaa5453e432c6e46099b8aa5a921
Thauan <dev.thauan.rodrigues@gmail.com>
2025-01-12 16:14:33 -0300

    crud com sql puro feito em Go

--- Pacotes Importantes/12- Banco de Dados/1/main.go
+++ Pacotes Importantes/12- Banco de Dados/1/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/google/uuid"
@@ -34,6 +35,38 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	product.Price = 100.10
+
+	err = updateProduct(db, product)
+
+	if err != nil {
+		panic(err)
+	}
+
+	// p, err := selectProduct(db, product.ID)
+
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// fmt.Printf("Produto: %v, possui %.2f de preço \n", p.Name, p.Price)
+
+	products, err := selectAllProducts(db)
+
+	if err != nil {
+		panic(err)
+	}
+
+	for _, p := range products {
+		fmt.Printf("O preço do produto %v é %.2f\n", p.Name, p.Price)
+	}
+
+	err = delete(db, product.ID)
+
+	if err != nil {
+		panic(err)
+	}
 }
 
 func insertProduct(db *sql.DB, product *Product) error {
@@ -44,6 +77,74 @@ func insertProduct(db *sql.DB, product *Product) error {
 	defer stmt.Close()
 
 	_, err = stmt.Exec(product.ID, product.Name, product.Price)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func updateProduct(db *sql.DB, product *Product) error {
+	stmt, err := db.Prepare("update products set name = ?, price = ? where id = ?")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(product.Name, product.Price, product.ID)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func selectProduct(db *sql.DB, id string) (*Product, error) {
+	stmt, err := db.Prepare("select id, name, price from products where id = ?")
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+
+	var p Product
+
+	err = stmt.QueryRow(id).Scan(&p.ID, &p.Name, &p.Price)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &p, nil
+}
+
+func selectAllProducts(db *sql.DB) ([]Product, error) {
+	rows, err := db.Query("select id, name, price from products")
+
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var products []Product
+	for rows.Next() {
+		var p Product
+		err = rows.Scan(&p.ID, &p.Name, &p.Price)
+		if err != nil {
+			return nil, err
+		}
+		products = append(products, p)
+	}
+	return products, nil
+}
+
+func delete(db *sql.DB, id string) error {
+	stmt, err := db.Prepare("delete from products where id= ?")
+	if err != nil {
+		return nil
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(id)
+
 	if err != nil {
 		return err
 	}
